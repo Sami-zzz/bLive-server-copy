6e5a5a1e25a2281d4d05abbacae7fb4b167f23f6
shuisheng <2274751790@qq.com>
2023-08-16 23:45:18 +0800

    feat: 2023-08-16webrtc一对一会议

--- src/config/websocket/index.ts
+++ src/config/websocket/index.ts
@@ -1,24 +1,48 @@
 import { Server } from 'socket.io';
 
+import {
+  WsAnswerType,
+  WsJoinType,
+  WsMsgTypeEnum,
+  WsOfferType,
+} from '@/interface-ws';
 import { chalkSUCCESS, chalkWARN } from '@/utils/chalkTip';
 
 export const connectWebSocket = (server) => {
   const io = new Server(server);
   console.log(chalkSUCCESS('初始化websocket成功！'));
 
-  io.on('connection', (socket) => {
+  // 每次链接都会触发connection
+  io.on(WsMsgTypeEnum.connection, (socket) => {
     console.log(chalkWARN('收到connection'), socket.id);
-  });
-  io.on('offer', (socket) => {
-    console.log('收到offer', socket);
-  });
-  io.on('answer', (socket) => {
-    console.log('收到answer', socket);
-  });
-  io.on('joined', (socket) => {
-    console.log('收到joined', socket);
-  });
-  io.on('message', (socket) => {
-    console.log('收到message', socket);
+
+    socket.on(WsMsgTypeEnum.join, (data: WsJoinType) => {
+      console.log('收到join', data);
+      socket.join(data.data.room_id);
+      socket.emit(WsMsgTypeEnum.joined, data);
+    });
+
+    socket.on(WsMsgTypeEnum.offer, (data: WsOfferType) => {
+      console.log('收到offer', data);
+      socket.to(data.data.room_id).emit(WsMsgTypeEnum.offer, data);
+    });
+
+    socket.on(WsMsgTypeEnum.answer, (data: WsAnswerType) => {
+      console.log('收到answer', data);
+      socket.to(data.data.room_id).emit(WsMsgTypeEnum.answer, data);
+    });
+
+    socket.on(WsMsgTypeEnum.candidate, (data: WsAnswerType) => {
+      console.log('收到candidate', data);
+      socket.to(data.data.room_id).emit(WsMsgTypeEnum.candidate, data);
+    });
+
+    socket.on(WsMsgTypeEnum.joined, (data) => {
+      console.log('收到joined', data);
+    });
+
+    socket.on(WsMsgTypeEnum.message, (data) => {
+      console.log('收到message', data);
+    });
   });
 };
